074ba1cf5f92939d6d049e34e91df2feeeef0f67
rick <rickbthomason@yahoo.com>
2022-04-21 11:07:30 -0400

    create a way to turn coffee maker off

--- Day15/coffee_maker.py
+++ Day15/coffee_maker.py
@@ -1,4 +1,20 @@
-from data import MENU
+from data import MENU, resources
 
-espresso = MENU['espresso']
-print(espresso['ingredients'])
+# TODO: 1. Print a report of the all the coffee machine resources
+report = f'Water: {resources["water"]}ml\nMilk: {resources["milk"]}ml\nCoffee: {resources["coffee"]}g'
+
+is_off = False
+
+# TODO: 2. Ask user what choice they would prefer
+while not is_off:
+    choice = input('Would you like an espresso/latte/cappuccino? ').lower()
+
+    if choice == 'report':
+        print(report)
+
+    # Turns coffee maker off
+    if choice == 'off':
+        is_off = True
+
+
+
