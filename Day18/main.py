a30d49ee51a43711ec7b5250887d7593a23e2bf7
rick <rickbthomason@yahoo.com>
2022-04-27 19:42:52 -0400

    create basic turtle object

---
+++ Day18/main.py
@@ -0,0 +1,15 @@
+from turtle import Screen, Turtle
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.color('green')
+timmy_the_turtle.shape('turtle')
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
