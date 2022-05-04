4d93e27e9f67f792281f003c57f826bf2314fdb4
rick <rickbthomason@yahoo.com>
2022-05-04 14:07:16 -0400

    create funciton to make turtle form a square

--- Day18/main.py
+++ Day18/main.py
@@ -1,15 +1,19 @@
 from turtle import Screen, Turtle
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.color('green')
-timmy_the_turtle.shape('turtle')
+timmy = Turtle()
+timmy.color('dark orange')
+timmy.shape('turtle')
+timmy.speed(2.5)
 
 
+# function to make the turtle form a square
+def turtle_square():
+    for _ in range(4):
+        timmy.forward(100)
+        timmy.right(90)
 
 
-
-
-
+turtle_square()
 
 screen = Screen()
 screen.exitonclick()
