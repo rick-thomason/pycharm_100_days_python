4490fb3fe54250ff026619ea0bcce3c6205a0dac
rick <rickbthomason@yahoo.com>
2022-05-04 14:59:21 -0400

    create loop to change turtle color and make multiple shapes

--- Day18/main.py
+++ Day18/main.py
@@ -1,9 +1,12 @@
 from turtle import Screen, Turtle
+import random
 
 timmy = Turtle()
-timmy.color('dark orange')
 timmy.shape('turtle')
-timmy.speed(2.5)
+timmy.speed(10)
+timmy.pensize(3)
+
+colors = ['medium aquamarine', 'dark orange', 'green', 'blue', 'purple', 'yellow', 'pink', 'brown']
 
 
 # function to make the turtle form a square
@@ -13,15 +16,28 @@ def turtle_square():
         timmy.right(90)
 
 
-def turtle_dash_line():
-    for _ in range(10):
+# function to create a dashed line
+def turtle_dash_line(num_dashes):
+    for _ in range(num_dashes):
         timmy.pd()
         timmy.forward(10)
         timmy.pu()
         timmy.forward(10)
 
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    timmy.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+
 # turtle_square()
-turtle_dash_line()
+# turtle_dash_line(20)
 screen = Screen()
 screen.exitonclick()
