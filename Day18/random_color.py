3aea0b99e698b53d2aa112c241f3e415db35d56a
rick <rickbthomason@yahoo.com>
2022-05-04 15:46:18 -0400

    create random color file and create turtle

---
+++ Day18/random_color.py
@@ -0,0 +1,12 @@
+import turtle
+import random
+
+turtle.colormode(255)
+
+timmy = turtle.Turtle()
+
+
+
+
+screen = turtle.Screen()
+screen.exitonclick()
--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,16 +1,18 @@
 from turtle import Turtle, Screen
+import random
 
 timmy = Turtle()
 timmy.color('medium aquamarine')
-timmy.shape('turtle')
-timmy.pensize(4)
+timmy.pensize(15)
+timmy.speed(0)  # fastest speed
 
 colors = ['medium aquamarine', 'dark orange', 'green', 'blue', 'purple', 'yellow', 'pink', 'brown']
+directions = [0, 90, 180, 270]
 
-
-
-
-
+for _ in range(200):
+    timmy.color(random.choice(colors))
+    timmy.forward(50)
+    timmy.setheading(random.choice(directions))
 
 
 screen = Screen()
