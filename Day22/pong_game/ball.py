3a81bdf64a033070374dd6494a183026c0113b83
rick <rickbthomason@yahoo.com>
2022-05-10 14:44:08 -0400

    create reset position method for when paddle misses ball

--- Day22/pong_game/ball.py
+++ Day22/pong_game/ball.py
@@ -15,8 +15,12 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(x=new_x, y=new_y)
 
-    def bounce_off_wall(self):
+    def bounce_y(self):
         self.y_move *= -1
 
-    def hit_paddle(self):
+    def bounce_x(self):
         self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
--- Day22/pong_game/main.py
+++ Day22/pong_game/main.py
@@ -19,22 +19,27 @@ screen.onkey(fun=left_paddle.move_down, key='s')
 screen.onkey(fun=right_paddle.move_up, key='Up')
 screen.onkey(fun=right_paddle.move_down, key='Down')
 
+speed = 0.1
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(speed)
     screen.update()
     ball.move()
 
     # Detect collision with the top and bottom walls
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce_off_wall()
+        ball.bounce_y()
 
-    # Detect collision with right paddle
-    if ball.xcor() > 340 and ball.distance(right_paddle) < 50:
-        ball.hit_paddle()
+    # Detect collision with paddles
+    if ball.xcor() > 320 and ball.distance(right_paddle) < 50 or ball.xcor() < -320 and ball.distance(left_paddle) < 50:
+        ball.bounce_x()
 
-    # Detect collision with left paddle
-    if ball.xcor() < -340 and ball.distance(left_paddle) < 50:
-        ball.hit_paddle()
+    # Detect if ball goes past right paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect if ball goes past left paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
