b06cf8d7770ebf91c8b418d6bd492545af370e61
rick <rickbthomason@yahoo.com>
2022-05-10 18:53:15 -0400

    write logic for when player collides with cars

--- Day23/turtle_crossing/main.py
+++ Day23/turtle_crossing/main.py
@@ -8,12 +8,12 @@ screen.title('Turtle Crossing')
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-leo = Player()
+player = Player()
 car_manager = CarManager()
 
 
 screen.listen()
-screen.onkeypress(fun=leo.move, key='Up')
+screen.onkeypress(fun=player.move, key='Up')
 
 game_is_on = True
 while game_is_on:
@@ -22,5 +22,10 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect if turtle collides with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 25:
+            game_is_on = False
 
 screen.exitonclick()
