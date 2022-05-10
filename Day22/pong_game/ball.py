65ad46373a065977c12aa76ea0dfc3f6d998d1a7
rick <rickbthomason@yahoo.com>
2022-05-10 15:35:00 -0400

    give ball class a move speed attribute so that the ball speed increases every time it bounces off a paddle

--- Day22/pong_game/ball.py
+++ Day22/pong_game/ball.py
@@ -21,7 +21,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
