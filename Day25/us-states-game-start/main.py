b39ae36e0258bacfe708577728a041512bb8e9b1
rick <rickbthomason@yahoo.com>
2022-05-14 09:13:55 -0400

    use gif to create new turtle shape

--- Day25/us-states-game-start/main.py
+++ Day25/us-states-game-start/main.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import turtle
 
 states = pd.read_csv('50_states.csv')
-print(states)
+image = 'blank_states_img.gif'
+
+screen = turtle.Screen()
+screen.title('U.S. States Game')
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+screen.exitonclick()
+
+
+
+
+
+
