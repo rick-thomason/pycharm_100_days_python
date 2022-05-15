05ed67220d57ba3006189da181d87080ec884fb1
rick <rickbthomason@yahoo.com>
2022-05-15 11:42:38 -0400

    learn to loop through a pandas dataframe

--- Day26/main.py
+++ Day26/main.py
@@ -1,15 +1,37 @@
+import random
+import pandas as pd
+
 # List Comprehension
 numbers = [1, 2, 3]
 new_list = [n + 1 for n in numbers]
-print(new_list)
 
 name = 'roland'.title()
 letters = [letter for letter in name]
-print(letters)
 
 doubled = [num * 2 for num in range(1, 5)]
-print(doubled)
 
 names = ['Amy', 'Beth', 'Rick', 'Caroline', 'Eleanor', 'Maddox']
 long_and_upper = [name.upper() for name in names if len(name) > 4]
-print(long_and_upper)
+
+# Dictionary Comprehension
+student_scores = {name: random.randint(50, 100) for name in names}
+# print(student_scores)
+
+passed_students = {student: score for (student, score) in student_scores.items() if score > 70}
+# print(passed_students)
+
+# Looping through a pandas dataframe
+student_dict = {
+    'student': ['Rick', 'Roland', 'Maddox'],
+    'score': [77, 88, 99]
+}
+
+student_df = pd.DataFrame(student_dict)
+# print(student_df)
+
+# for (key, value) in student_df.items():
+#     print(key)
+
+# Loop through the rows of the dataframe
+for (index, row) in student_df.iterrows():
+    print(row['student'])
