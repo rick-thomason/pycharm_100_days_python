b5d7bd340a3257cd30624a5a75223ec75b6c82f1
rick <rickbthomason@yahoo.com>
2022-05-15 18:34:46 -0400

    create a window with tkinter and create label, button, and entry in window

--- Day27/main.py
+++ Day27/main.py
@@ -4,6 +4,24 @@ window = tk.Tk()
 window.title('MY First GUI Program')
 window.minsize(width=500, height=300)
 
+# Create a Label
+my_label = tk.Label(text='My First Label', fg='black', font=('Fira Code', 20))
+my_label.pack()
+
+# Create a Button
+
+
+def button_clicked():
+    new_input = user_input.get()
+    my_label.config(text=new_input)
+
+
+my_button = tk.Button(text='Click Me', command=button_clicked)
+my_button.pack()
+
+# Create Entry Component
+user_input = tk.Entry(bg='white', fg='black', width=10)
+user_input.pack()
 
 
 window.mainloop()
