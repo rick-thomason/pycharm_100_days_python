5ef25f8a17d572a1344d12932c8fa3ab02dfa86f
rick <rickbthomason@yahoo.com>
2022-05-16 16:09:43 -0400

    create canvas and add tomato image to it

--- Day28/pomodoro/main.py
+++ Day28/pomodoro/main.py
@@ -1,4 +1,4 @@
-
+import tkinter as tk
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title('Pomodoro Timer')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = tk.PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=image)
+canvas.create_text(110, 130, text='00:00', font=(FONT_NAME, 35, 'bold'))
+canvas.pack()
+
+window.mainloop()
