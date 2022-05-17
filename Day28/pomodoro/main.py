5df13540a4ce53b584322e666a2d7a67965871c4
rick <rickbthomason@yahoo.com>
2022-05-17 15:46:57 -0400

    write logic in start timer to go through work time and break times

--- Day28/pomodoro/main.py
+++ Day28/pomodoro/main.py
@@ -5,9 +5,10 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- # 
@@ -17,14 +18,40 @@ def reset_timer():
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text='Break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text='Break', fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text='Work', fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    min_count = count // 60
+    sec_count = count % 60
+
+    if sec_count < 10:
+        sec_count = f'0{sec_count}'
+
+    if min_count < 10:
+        min_count = f'0{min_count}'
+
+    canvas.itemconfig(timer_text, text=f'{min_count}:{sec_count}')
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
