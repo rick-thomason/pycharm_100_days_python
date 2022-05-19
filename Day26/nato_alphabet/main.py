8cec42727137c215da480f085950b5c6a9b7041e
rick <rickbthomason@yahoo.com>
2022-05-19 19:11:31 -0400

    create a function to check for errors and keep asking for a word if one is not given

--- Day26/nato_alphabet/main.py
+++ Day26/nato_alphabet/main.py
@@ -4,9 +4,17 @@ nato_df = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 # print(nato_dict)
 
+
 # #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter a word: ').upper()
+def generate_phonetic():
+    word = input('Enter a word: ').upper()
+    try:
+        phonetic_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry, only letters in the alphabet.')
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
-phonetic_list = [nato_dict[letter] for letter in word]
 
-print(phonetic_list)
+generate_phonetic()
