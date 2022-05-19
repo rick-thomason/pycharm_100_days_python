e145203de176ecfa04a5de6af7f3cc387b1955be
rick <rickbthomason@yahoo.com>
2022-05-19 16:13:32 -0400

    create generate password funciton to create a random password

--- Day29/password-manager/main.py
+++ Day29/password-manager/main.py
@@ -1,10 +1,34 @@
 import tkinter as tk
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_input.insert(0, password)
+    # print(f"Your password is: {password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -18,7 +42,7 @@ def save_data():
         messagebox.showwarning(title='Oooops!', message='You must fill in all fields')
     else:
         is_ok = messagebox.askokcancel(title=website_data,
-                                       message=f'These are the details that were entered:\nEmail:{email_data}'
+                                       message=f'These are the details that were entered:\nEmail: {email_data}'
                                                f'\nPassword: {password_data}\nIs it ok to save?')
 
         if is_ok:
@@ -26,8 +50,6 @@ def save_data():
                 file.write(f'{website_data} | {email_data} | {password_data}\n')
                 website_input.delete(0, 'end')
                 password_input.delete(0, 'end')
-
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
