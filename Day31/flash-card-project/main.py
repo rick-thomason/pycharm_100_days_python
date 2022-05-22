bc732176bc5d49095706983867bc69bbeb5927d3
rick <rickbthomason@yahoo.com>
2022-05-22 13:31:49 -0400

    create flip card function that shows the english form of the french word after 3 seconds

--- Day31/flash-card-project/main.py
+++ Day31/flash-card-project/main.py
@@ -1,32 +1,41 @@
 import tkinter as tk
 import pandas as pd
 import random
+import time
 
+BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv('data/french_words.csv')
 to_learn = data.to_dict(orient='records')
-print(to_learn)
+current_card = {}
 
 
 # function to call next card when button is clicked
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text='French')
     canvas.itemconfig(card_word, text=current_card['French'])
 
 
-BACKGROUND_COLOR = "#B1DDC6"
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_word, text=current_card['English'], fill='white')
+    canvas.itemconfig(card_background, image=card_back_image)
+
 
 window = tk.Tk()
 window.title('Flashy')
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+window.after(3000, func=flip_card)
+
 # Canvas
 canvas = tk.Canvas(width=800, height=526)
 card_front_image = tk.PhotoImage(file='images/card_front.png')
 card_back_image = tk.PhotoImage(file='images/card_back.png')
-canvas.create_image(400, 263, image=card_front_image)
-card_title = canvas.create_text(400, 150, text='Title', font=('Arial', 40, 'italic'), fill='black')
-card_word = canvas.create_text(400, 263, text='word', font=('Arial', 60, 'bold'), fill='black')
+card_background = canvas.create_image(400, 263, image=card_front_image)
+card_title = canvas.create_text(400, 150, font=('Arial', 40, 'italic'), fill='black')
+card_word = canvas.create_text(400, 263, font=('Arial', 60, 'bold'), fill='black')
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(columnspan=2, column=0, row=0)
 
